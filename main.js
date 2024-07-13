f577377000c0a80ed5a4207c31a77927430fccc1
Enrique1311 <ejspinelli131176@gmail.com>
2024-07-13 19:28:44 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Navigator
+// *****************************************************************
+
+// Properties
+// ********************
+
+console.log("Connection:", navigator.connection);
+
+console.log("Geolocation:", navigator.geolocation);
+
+console.log("The mount of processor's cores :", navigator.hardwareConcurrency);
+
+console.log("Language:", navigator.language);
+
+console.log("Navigator languages", navigator.languages);
+
+console.log("Online confirmation: ", navigator.onLine);
+
+console.log("User information:", navigator.userAgent);
+
+console.log("Cookie enabled:", navigator.cookieEnabled);
+
+console.log("Permissions:", navigator.permissions);
+
+console.log("Service worker:", navigator.serviceWorker);
